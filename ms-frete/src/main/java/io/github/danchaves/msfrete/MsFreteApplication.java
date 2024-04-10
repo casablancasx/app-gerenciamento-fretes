c6246fa9d8bee525a6db195971be5a5635b6de1a
Danilo Chaves <danchavesds@gmail.com>
2024-04-10 15:02:20 -0300

    commit inicial

---
+++ ms-frete/src/main/java/io/github/danchaves/msfrete/MsFreteApplication.java
@@ -0,0 +1,13 @@
+package io.github.danchaves.msfrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFreteApplication.class, args);
+	}
+
+}
---
+++ ms-frete/src/test/java/io/github/danchaves/msfrete/MsFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danchaves.msfrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
